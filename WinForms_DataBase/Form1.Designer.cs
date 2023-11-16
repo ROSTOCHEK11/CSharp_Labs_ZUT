79c647e38194a41047bbb43bf789a37b8231428e
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2023-11-16 13:28:32 +0100

    Small DataBase on WIndows Forms

---
+++ WinForms_DataBase/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿using System.Windows.Forms;
+
+namespace Lab5WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbSurname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpBirth = new System.Windows.Forms.DateTimePicker();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.lbData = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(34, 42);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(266, 22);
+            this.tbName.TabIndex = 0;
+            // 
+            // tbSurname
+            // 
+            this.tbSurname.Location = new System.Drawing.Point(34, 98);
+            this.tbSurname.Name = "tbSurname";
+            this.tbSurname.Size = new System.Drawing.Size(266, 22);
+            this.tbSurname.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Birth date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Gender";
+            // 
+            // dtpBirth
+            // 
+            this.dtpBirth.CustomFormat = "yyyy-MM-dd";
+            this.dtpBirth.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpBirth.Location = new System.Drawing.Point(34, 154);
+            this.dtpBirth.Name = "dtpBirth";
+            this.dtpBirth.Size = new System.Drawing.Size(266, 22);
+            this.dtpBirth.TabIndex = 8;
+            // 
+            // cbGender
+            // 
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Other"});
+            this.cbGender.Location = new System.Drawing.Point(34, 214);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(266, 24);
+            this.cbGender.TabIndex = 9;
+            // 
+            // lbData
+            // 
+            this.lbData.FormattingEnabled = true;
+            this.lbData.ItemHeight = 16;
+            this.lbData.Location = new System.Drawing.Point(34, 304);
+            this.lbData.Name = "lbData";
+            this.lbData.Size = new System.Drawing.Size(266, 132);
+            this.lbData.TabIndex = 10;
+            this.lbData.SelectedIndexChanged += new System.EventHandler(this.lbData_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(122, 262);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(122, 460);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 495);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lbData);
+            this.Controls.Add(this.cbGender);
+            this.Controls.Add(this.dtpBirth);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSurname);
+            this.Controls.Add(this.tbName);
+            this.Name = "Form1";
+            this.Text = "DataBase";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbSurname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpBirth;
+        private System.Windows.Forms.ComboBox cbGender;
+        private System.Windows.Forms.ListBox lbData;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ WinForms_DataBase/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5WinForm
+{
+    public partial class Form1 : Form
+    {
+
+        private List<PersonData> DataList = new List<PersonData>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbData.DisplayMember = "FullName";
+            ListRenew();
+        }
+
+        private void ListRenew()
+        {
+            lbData.DataSource = null;
+            lbData.DataSource = DataList;
+            lbData.DisplayMember = "FullData";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = tbName.Text;
+            string surname = tbSurname.Text;
+            DateTime birthdate = dtpBirth.Value;
+            string gender = cbGender.Text;
+
+            if (!string.IsNullOrWhiteSpace(name) &&
+                !string.IsNullOrWhiteSpace(surname) &&
+                !string.IsNullOrWhiteSpace(gender))
+            {
+                // Tworzenie nowego obiektu danych osobowych i dodanie go do listy
+                PersonData person = new PersonData(name, surname, birthdate, gender);
+                DataList.Add(person);
+
+                ListRenew(); // Odświeżenie listy po dodaniu danych
+
+                // Wyczyszczenie pól tekstowych
+                tbName.Clear();
+                tbSurname.Clear();
+                cbGender.SelectedIndex = -1;
+                dtpBirth.Value = DateTime.Now;
+            }
+            else
+            {
+                MessageBox.Show("Wypełnij wszystkie pola!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            if (lbData.SelectedIndex != -1) // Sprawdzenie, czy jakiś rekord jest wybrany
+            {
+                DataList.RemoveAt(lbData.SelectedIndex); // Usunięcie wybranego rekordu z listy
+                ListRenew(); // Odświeżenie listy po usunięciu danych
+
+                // Wyczyszczenie pól tekstowych
+                tbName.Clear();
+                tbSurname.Clear();
+                cbGender.SelectedIndex = -1;
+                dtpBirth.Value = DateTime.Now;
+            }
+            else
+            {
+                MessageBox.Show("Wybierz rekord do usunięcia!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void lbData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lbData.SelectedIndex != -1) 
+            {
+
+                PersonData ChosenPerson = (PersonData)lbData.SelectedItem;
+                tbName.Text = ChosenPerson.Name;
+                tbSurname.Text = ChosenPerson.Surname;
+                cbGender.Text = ChosenPerson.Gender;
+                dtpBirth.Value = ChosenPerson.BirthDate;
+
+            }
+
+
+        }
+    }
+
+    public class PersonData
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+
+        public string FullName => Name + " " + Surname; // Właściwość do wyświetlania imienia i nazwiska
+        public string FullData => Name+ " " + Surname + ", " + BirthDate.Date.ToString("d") + ", " + Gender;
+
+        public PersonData(string name, string surname, DateTime birthdate, string gender)
+        {
+            Name = name;
+            Surname = surname;
+            BirthDate = birthdate;
+            Gender = gender;
+        }
+    }
+
+
+}
