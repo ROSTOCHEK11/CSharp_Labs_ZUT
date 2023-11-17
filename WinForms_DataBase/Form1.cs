43850170e2c6e562a91f4175ad3392348160ffb2
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2023-11-17 20:33:48 +0100

    Update Form1.cs, added new function

--- WinForms_DataBase/Form1.cs
+++ WinForms_DataBase/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,7 +22,7 @@ namespace Lab5WinForm
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            lbData.DisplayMember = "FullName";
+           
             ListRenew();
         }
 
@@ -99,6 +99,47 @@ namespace Lab5WinForm
 
 
         }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (lbData.SelectedIndex != -1) 
+            {
+                string name = tbName.Text;
+                string surname = tbSurname.Text;
+                DateTime birthdate = dtpBirth.Value;
+                string gender = cbGender.Text;
+
+                if (!string.IsNullOrWhiteSpace(name) &&
+                    !string.IsNullOrWhiteSpace(surname) &&
+                    !string.IsNullOrWhiteSpace(gender))
+                {
+                    
+                    PersonData selectedPerson = DataList[lbData.SelectedIndex];
+
+                    
+                    selectedPerson.Name = name;
+                    selectedPerson.Surname = surname;
+                    selectedPerson.BirthDate = birthdate;
+                    selectedPerson.Gender = gender;
+
+                    ListRenew();
+
+                    
+                    tbName.Clear();
+                    tbSurname.Clear();
+                    cbGender.SelectedIndex = -1;
+                    dtpBirth.Value = DateTime.Now;
+                }
+                else
+                {
+                    MessageBox.Show("Wypełnij wszystkie pola!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wybierz rekord do aktualizacji!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 
     public class PersonData
@@ -108,7 +149,7 @@ namespace Lab5WinForm
         public DateTime BirthDate { get; set; }
         public string Gender { get; set; }
 
-        public string FullName => Name + " " + Surname; // Właściwość do wyświetlania imienia i nazwiska
+       
         public string FullData => Name+ " " + Surname + ", " + BirthDate.Date.ToString("d") + ", " + Gender;
 
         public PersonData(string name, string surname, DateTime birthdate, string gender)
