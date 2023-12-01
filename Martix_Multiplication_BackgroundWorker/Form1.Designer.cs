23aade1dd120c55e3a2a7f54fda12cfc784c69b9
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2023-12-01 10:51:42 +0100

    Matrix Multiplication with BackgroundWorker

---
+++ Martix_Multiplication_BackgroundWorker/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace lab7_backgroundWorker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBoxRows1 = new System.Windows.Forms.TextBox();
+            this.textBoxCols1 = new System.Windows.Forms.TextBox();
+            this.textBoxRows2 = new System.Windows.Forms.TextBox();
+            this.textBoxCols2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(90, 359);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(626, 38);
+            this.progressBar.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Multiply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.multiplyButton_Click);
+            // 
+            // textBoxRows1
+            // 
+            this.textBoxRows1.Location = new System.Drawing.Point(128, 105);
+            this.textBoxRows1.Name = "textBoxRows1";
+            this.textBoxRows1.Size = new System.Drawing.Size(100, 22);
+            this.textBoxRows1.TabIndex = 2;
+            // 
+            // textBoxCols1
+            // 
+            this.textBoxCols1.Location = new System.Drawing.Point(128, 148);
+            this.textBoxCols1.Name = "textBoxCols1";
+            this.textBoxCols1.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCols1.TabIndex = 3;
+            // 
+            // textBoxRows2
+            // 
+            this.textBoxRows2.Location = new System.Drawing.Point(538, 104);
+            this.textBoxRows2.Name = "textBoxRows2";
+            this.textBoxRows2.Size = new System.Drawing.Size(100, 22);
+            this.textBoxRows2.TabIndex = 4;
+            // 
+            // textBoxCols2
+            // 
+            this.textBoxCols2.Location = new System.Drawing.Point(538, 148);
+            this.textBoxCols2.Name = "textBoxCols2";
+            this.textBoxCols2.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCols2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "rows";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "cols";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(497, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "rows";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(500, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "cols";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(147, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Matrix 1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(555, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Matrix 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCols2);
+            this.Controls.Add(this.textBoxRows2);
+            this.Controls.Add(this.textBoxCols1);
+            this.Controls.Add(this.textBoxRows1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBoxRows1;
+        private System.Windows.Forms.TextBox textBoxCols1;
+        private System.Windows.Forms.TextBox textBoxRows2;
+        private System.Windows.Forms.TextBox textBoxCols2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Martix_Multiplication_BackgroundWorker/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab7_backgroundWorker
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker matrixWorker = new BackgroundWorker();
+        private int[,] resultMatrix;
+
+        public Form1()
+        {
+            InitializeComponent();
+            matrixWorker.WorkerReportsProgress = true;
+            matrixWorker.DoWork += MatrixWorker_DoWork;
+            matrixWorker.ProgressChanged += MatrixWorker_ProgressChanged;
+            matrixWorker.RunWorkerCompleted += MatrixWorker_RunWorkerCompleted;
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            int rowsA = int.Parse(textBoxRows1.Text);
+            int colsA = int.Parse(textBoxCols1.Text);
+            int rowsB = int.Parse(textBoxRows2.Text);
+            int colsB = int.Parse(textBoxCols2.Text);
+
+            // For simplicity, creating random matrices A and B with specified sizes
+            int[,] matrixA = GenerateRandomMatrix(rowsA, colsA);
+            int[,] matrixB = GenerateRandomMatrix(rowsB, colsB);
+
+            progressBar.Minimum = 0;
+            progressBar.Maximum = rowsA;
+
+            matrixWorker.RunWorkerAsync(new Tuple<int[,], int[,]>(matrixA, matrixB));
+        }
+
+        private int[,] GenerateRandomMatrix(int rows, int cols)
+        {
+            Random rand = new Random();
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rand.Next(1, 10); // Filling with random integers (change range as needed)
+                }
+            }
+
+            return matrix;
+        }
+
+        private void MatrixWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int[,] matrixA = ((Tuple<int[,], int[,]>)e.Argument).Item1;
+            int[,] matrixB = ((Tuple<int[,], int[,]>)e.Argument).Item2;
+
+            int rowsA = matrixA.GetLength(0);
+            int colsB = matrixB.GetLength(1);
+
+            resultMatrix = new int[rowsA, colsB];
+
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int j = 0; j < colsB; j++)
+                {
+                    long sum = 0; // Use long for large matrices
+                    for (int k = 0; k < matrixA.GetLength(1); k++)
+                    {
+                        sum += (long)matrixA[i, k] * matrixB[k, j]; // Use long for intermediate values
+                    }
+                    resultMatrix[i, j] = (int)sum;
+
+                    // Report progress for each row multiplication
+                    matrixWorker.ReportProgress(i + 1);
+                }
+            }
+        }
+
+        private void MatrixWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void MatrixWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // Use the resulting matrix, display it, or perform any necessary action
+            MessageBox.Show("Matrix multiplication completed!");
+        }
+
+
+    }
+}
