c4822f3d57d993cd297d488fbe55621f13a05b43
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2023-12-03 23:28:24 +0100

    Custom Timer on C# WIndows Forms

---
+++ CustomTimer_WinForms/CustomTimer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using Timer = System.Threading.Timer;
+
+namespace CustomTimer_WinForms
+{
+    public class CustomTimer
+    {
+        private System.Threading.Timer timer;
+        private bool isRunning;
+        private int interval;
+        private Action timeElapsedHandler;
+        private Action<Exception> errorHandler;
+
+
+        public event Action TimeElapsed
+        {
+            add { timeElapsedHandler += value; }
+            remove { timeElapsedHandler -= value; }
+        }
+
+        public event Action<Exception> OnError
+        {
+            add { errorHandler += value; }
+            remove { errorHandler -= value; }
+        }
+
+        public CustomTimer(int interval)
+        {
+            this.interval = interval;
+            isRunning = false;
+        }
+
+        public void Start()
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                timer = new System.Threading.Timer(TimerCallback, null, 0, interval);
+            }
+        }
+
+        public void Stop()
+        {
+            if (isRunning)
+            {
+                timer.Dispose();
+                isRunning = false;
+            }
+        }
+
+        private void TimerCallback(object state)
+        {
+            try
+            {
+                // Simulating some task execution in the elapsed time event
+                ThreadPool.QueueUserWorkItem(callbackState =>
+                {
+                    timeElapsedHandler?.Invoke();
+                });
+            }
+            catch (Exception ex)
+            {
+                if (errorHandler != null)
+                {
+                    errorHandler(ex); // Invoke the errorHandler delegate
+                }
+                // Alternatively, you can use: errorHandler?.Invoke(ex);
+            }
+        }
+    }
+
+
+
+}
---
+++ CustomTimer_WinForms/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomTimer_WinForms
+{
+    public partial class Timer : Form
+    {
+        private CustomTimer customTimer;
+
+        public Timer()
+        {
+            InitializeComponent();
+            customTimer = new CustomTimer(1000); // Interval set to 1000ms (1 second)
+            customTimer.TimeElapsed += CustomTimer_TimeElapsed;
+            customTimer.OnError += CustomTimer_OnError;
+        }
+
+        private void CustomTimer_TimeElapsed()
+        {
+            // Handle time elapsed event here
+            Invoke((Action)delegate
+            {
+                // Update UI or perform actions
+                label1.Text = DateTime.Now.ToString("HH:mm:ss");
+            });
+        }
+
+        private void CustomTimer_OnError(Exception ex)
+        {
+            // Handle error event here
+            MessageBox.Show($"Error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            customTimer.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            customTimer.Stop();
+        }
+    }
+
+
+
+}
