6335f2065351459f5f2daa25a77eec754a2d3d02
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2023-12-03 23:30:10 +0100

    Update CustomTimer.cs

--- CustomTimer_WinForms/CustomTimer.cs
+++ CustomTimer_WinForms/CustomTimer.cs
@@ -66,7 +66,7 @@ namespace CustomTimer_WinForms
                 {
                     errorHandler(ex); // Invoke the errorHandler delegate
                 }
-                // Alternatively, you can use: errorHandler?.Invoke(ex);
+                
             }
         }
     }
