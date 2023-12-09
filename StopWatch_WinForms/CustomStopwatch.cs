a9d1faa0e1f690f01b6ba23ec3f7e2999e2efcd1
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2023-12-09 20:34:41 +0100

    Custom StopWatch on WinForms
    Custom StopWatch on WinForms without using Timer library

---
+++ StopWatch_WinForms/CustomStopwatch.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StopWatch_WinForms
+{
+    public class CustomStopwatch
+    {
+        private Thread timerThread;
+        private bool isRunning;
+        private DateTime lastStartTime;
+        private TimeSpan elapsedTime;
+        private readonly object lockObject = new object();
+
+        public event Action<TimeSpan> TimeElapsed;
+        public event Action<Exception> OnError;
+
+        public CustomStopwatch()
+        {
+            elapsedTime = TimeSpan.Zero;
+            isRunning = false;
+        }
+
+        public void Start()
+        {
+            lock (lockObject)
+            {
+                if (isRunning) return;
+
+                isRunning = true;
+                lastStartTime = DateTime.Now;
+                if (timerThread == null || !timerThread.IsAlive)
+                {
+                    timerThread = new Thread(RunTimer) { IsBackground = true };
+                    timerThread.Start();
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            lock (lockObject)
+            {
+                if (!isRunning) return;
+
+                elapsedTime += DateTime.Now - lastStartTime;
+                isRunning = false;
+                TimeElapsed?.Invoke(elapsedTime);
+            }
+        }
+
+        public void Reset()
+        {
+            lock (lockObject)
+            {
+                isRunning = false;
+                elapsedTime = TimeSpan.Zero;
+                TimeElapsed?.Invoke(TimeSpan.Zero);
+            }
+        }
+
+        private void RunTimer()
+        {
+            try
+            {
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    lock (lockObject)
+                    {
+                        if (isRunning)
+                        {
+                            var currentElapsedTime = elapsedTime + (DateTime.Now - lastStartTime);
+                            TimeElapsed?.Invoke(currentElapsedTime);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                OnError?.Invoke(ex);
+            }
+        }
+
+
+    }
+
+}
---
+++ StopWatch_WinForms/Form1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StopWatch_WinForms
+{
+    public partial class Form1 : Form
+    {
+        private CustomStopwatch stopwatch;
+
+        public Form1()
+        {
+            InitializeComponent();
+            stopwatch = new CustomStopwatch();
+            stopwatch.TimeElapsed += UpdateTimeLabel;
+            stopwatch.OnError += HandleError;
+        }
+
+
+        private void UpdateTimeLabel(TimeSpan time)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<TimeSpan>(UpdateTimeLabel), time);
+            }
+            else
+            {
+                label1.Text = time.ToString(@"hh\:mm\:ss");
+            }
+        }
+
+        private void HandleError(Exception ex)
+        {
+            MessageBox.Show($"Error: {ex.Message}");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            stopwatch.Stop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            stopwatch.Reset();
+        }
+
+
+
+
+    }
+
+
+}
