89381363cfdf11c15cd974f2b0e1d55dfeb20c8b
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2024-01-19 01:03:24 +0100

    HttpServer
    Simple Web-Server in c# using WF

---
+++ HttpServer/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace HttpServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.file_txt = new System.Windows.Forms.TextBox();
+            this.serverLogs_txt = new System.Windows.Forms.TextBox();
+            this.port_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.file_btn = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(35, 233);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(88, 39);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(155, 233);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(88, 39);
+            this.stop_btn.TabIndex = 1;
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // file_txt
+            // 
+            this.file_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.file_txt.Location = new System.Drawing.Point(35, 171);
+            this.file_txt.Name = "file_txt";
+            this.file_txt.Size = new System.Drawing.Size(231, 28);
+            this.file_txt.TabIndex = 2;
+            // 
+            // serverLogs_txt
+            // 
+            this.serverLogs_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.serverLogs_txt.Location = new System.Drawing.Point(488, 82);
+            this.serverLogs_txt.Multiline = true;
+            this.serverLogs_txt.Name = "serverLogs_txt";
+            this.serverLogs_txt.ReadOnly = true;
+            this.serverLogs_txt.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.serverLogs_txt.Size = new System.Drawing.Size(503, 467);
+            this.serverLogs_txt.TabIndex = 3;
+            // 
+            // port_txt
+            // 
+            this.port_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.port_txt.Location = new System.Drawing.Point(35, 95);
+            this.port_txt.Name = "port_txt";
+            this.port_txt.Size = new System.Drawing.Size(231, 28);
+            this.port_txt.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(35, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(35, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "File";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(483, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Server Logs";
+            // 
+            // file_btn
+            // 
+            this.file_btn.Location = new System.Drawing.Point(272, 168);
+            this.file_btn.Name = "file_btn";
+            this.file_btn.Size = new System.Drawing.Size(75, 28);
+            this.file_btn.TabIndex = 8;
+            this.file_btn.Text = "File";
+            this.file_btn.UseVisualStyleBackColor = true;
+            this.file_btn.Click += new System.EventHandler(this.file_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 653);
+            this.Controls.Add(this.file_btn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.port_txt);
+            this.Controls.Add(this.serverLogs_txt);
+            this.Controls.Add(this.file_txt);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.start_btn);
+            this.Name = "Form1";
+            this.Text = "Http Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.TextBox file_txt;
+        private System.Windows.Forms.TextBox serverLogs_txt;
+        private System.Windows.Forms.TextBox port_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button file_btn;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ HttpServer/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace HttpServer
+{
+    public partial class Form1 : Form
+    {
+        private FolderBrowserDialog folderBrowserDialog;
+
+        private TcpListener httpServer;
+        private Thread serverThread;
+        private string servingDirectory;
+        private int port;
+
+        public Form1()
+        {
+            InitializeComponent();
+            stop_btn.Enabled = false;
+
+            folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            serverLogs_txt.Text = "";
+
+            try
+            {
+                port = int.Parse(port_txt.Text);
+                servingDirectory = file_txt.Text;
+
+                if (!Directory.Exists(servingDirectory))
+                {
+                    throw new Exception("The specified directory does not exist.");
+                }
+
+                httpServer = new TcpListener(IPAddress.Any, port);
+                serverThread = new Thread(new ThreadStart(StartServer));
+                serverThread.Start();
+
+                start_btn.Enabled = false;
+                stop_btn.Enabled = true;
+
+                LogToServerLogs("Server started successfully on port " + port);
+            }
+            catch (Exception ex)
+            {
+                LogToServerLogs("Error starting server: " + ex.Message);
+            }
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (httpServer != null)
+                {
+                    httpServer.Stop();
+                    serverThread?.Abort();
+
+                    start_btn.Enabled = true;
+                    stop_btn.Enabled = false;
+
+                    LogToServerLogs("Server stopped successfully.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                LogToServerLogs("Error stopping server: " + ex.Message);
+            }
+        }
+
+        private void StartServer()
+        {
+            try
+            {
+                httpServer.Start();
+
+                while (true)
+                {
+                    TcpClient client = httpServer.AcceptTcpClient();
+                    Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+                    clientThread.Start(client);
+                }
+            }
+            catch (Exception ex)
+            {
+                Invoke(new Action(() => serverLogs_txt.Text += $"Server error: {ex.Message}\n"));
+            }
+        }
+
+        private void HandleClient(object obj)
+        {
+            TcpClient client = obj as TcpClient;
+            if (client == null)
+            {
+                // Log error or handle the case where client is null
+                return;
+            }
+
+            NetworkStream stream = null;
+
+            try
+            {
+
+                stream = client.GetStream();
+
+                if (stream == null)
+                {
+                    // Handle null stream
+                    return;
+                }
+
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string request = reader.ReadLine();
+
+                    // Check if the request is null or empty
+                    if (string.IsNullOrEmpty(request))
+                    {
+                        // Handle the case where request is null or empty
+                        return;
+                    }
+
+                    // Process the request
+                    if (request.StartsWith("GET"))
+                    {
+                        string[] tokens = request.Split(' ');
+                        string requestedFile = tokens[1].TrimStart('/');
+
+                        string filePath = Path.Combine(servingDirectory, string.IsNullOrEmpty(requestedFile) ? "index.html" : requestedFile);
+
+                        if (File.Exists(filePath))
+                        {
+                            SendResponse(stream, File.ReadAllText(filePath), "200 OK");
+                        }
+                        else
+                        {
+                            SendResponse(stream, "<html><body><h2>404 Not Found</h2></body></html>", "404 Not Found");
+                        }
+
+                        LogToServerLogs("Received request: " + request);
+                    }
+
+                    LogToServerLogs("Sent response for request: " + request);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                LogToServerLogs("Error in HandleClient: " + ex.Message);
+
+                if (stream != null)
+                {
+                    SendResponse(stream, "<html><body><h2>500 Internal Server Error</h2><p>An error occurred while processing your request.</p></body></html>", "500 Internal Server Error");
+                }
+            }
+            finally
+            {
+                stream.Close();
+                client.Close();
+            }
+        }
+
+        private void SendResponse(NetworkStream stream, string body, string statusCode)
+        {
+            StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine($"HTTP/1.1 {statusCode}");
+            writer.WriteLine("Content-Type: text/html; charset=utf-8");
+            writer.WriteLine("Connection: close");
+            writer.WriteLine("");
+            writer.WriteLine(body);
+            writer.Flush();
+        }
+
+
+        private void LogToServerLogs(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(LogToServerLogs), new object[] { message });
+            }
+            else
+            {
+                serverLogs_txt.AppendText(message + Environment.NewLine);
+            }
+        }
+
+        private void file_btn_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the selected folder's path
+                string folderPath = folderBrowserDialog.SelectedPath;
+
+                // Set the directory path to the selected folder
+                file_txt.Text = folderPath;
+            }
+        }
+    }
+}
